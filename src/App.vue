ac04ee6b4bb13cf5170d85cc8d9fe9cbb303cf8e
Luis Eduardo Herrera Gonzalez <u202218227@upc.edu.pe>
2024-04-25 17:36:44 -0500

    feat: implemented http common service to acquire all data, base api service and agents api service.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import {AgentsApiService} from "./shared/services/agents-api.service.js";
+
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      sidebarVisible: false,
+      agents: [],
+      errors: [],
+      agentsApi: new AgentsApiService(),
+    }
+  },
+  created() {
+    this.getAgents();
+  },
+  methods: {
+    // Fetch articles for selected Source
+    getAgents() {
+      this.agentsApi.getAll()
+          .then(response => {
+            this.agents = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            this.errors.push(e);
+          });
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
---
+++ src/shared/services/agents-api.service.js
@@ -0,0 +1,6 @@
+import http from '../../shared/services/http-common.js';
+import {BaseService} from "./base.service.js";
+
+export class AgentsApiService extends BaseService {
+    complementUrl = '/agents';
+}
